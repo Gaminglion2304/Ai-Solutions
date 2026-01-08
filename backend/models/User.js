88c9146fe5ea36005295815ff5aec58cbad86023
MOUSSET Luc <lucmousset@outlook.com>
2026-01-08 18:28:08 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,8 @@
+import mongoose from "mongoose";
 
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  key: { type: String, required: true }
+});
+
+export default mongoose.model("User", UserSchema);
