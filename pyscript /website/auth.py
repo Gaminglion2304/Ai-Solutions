4bd1d72fe523733ceb5e530bc90f582a6481a31f
Kaleb G <164186923+KGas1610@users.noreply.github.com>
2026-01-19 21:31:54 +0200

    Refactor sign-up email and password processing
    Updated email handling and password hashing method in sign-up.

--- pyscript /website/auth.py
+++ pyscript /website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db   
 from flask_login import login_user, login_required, logout_user, current_user
 
 
@@ -38,7 +38,7 @@ def logout():
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
-        email = request.form.get('email')
+        email = request.form.get('email').lower().strip()
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
@@ -55,8 +55,14 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(
+                email=email, 
+                first_name=first_name, 
+                password=generate_password_hash(
+                    password1, 
+                    method='pbkdf2:sha256'
+                )
+            )
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
